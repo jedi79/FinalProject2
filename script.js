30aeb2f88e45efde82b20dc57dd93f834ecbd54e
Jed Mannheimer <jedman1@mac.com>
2016-08-05 19:20:05 -0400

    changed design images and thumbnails

--- script.js
+++ script.js
@@ -12,17 +12,18 @@ $(document).ready(function() {
     }
   });
 
-  $('.legals').on("click",function(e){
+  $('.thumbs').on("click",function(e){
     e.preventDefault();
     console.log(e);
     var imgUrl = $(this).data("img-url");
-    $('#lbIMG').attr("src",imgUrl);
+    $('#lbIMG').attr("src", imgUrl);
     $('.lightbox').show();
     $('.lightbox').on("click",function(){
       $('.lightbox').hide();
     });
   });
 })
+
 
 
 /* $(document).ready(function(){
